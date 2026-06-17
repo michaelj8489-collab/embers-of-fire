da789122a5b4bb0406a322efdde60fd4f712d035
Justin <j.cox1989@outlook.com>
2026-06-17 12:31:01 -0400

    Add Rise Awakenings embedded player

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://vchyjroofvqmgdlbydhp.supabase.co https://zenoimages.s3.us-west-001.backblazeb2.com https://*.giphy.com https://images-api.printify.com;
+  connect-src 'self' https://vchyjroofvqmgdlbydhp.supabase.co https://api.stripe.com;
+  frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://zeno.fm;
+`.replace(/\s{2,}/g, ' ').trim();
+
 const nextConfig: NextConfig = {
   images: {
     minimumCacheTTL: 31536000,
@@ -22,10 +31,40 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'images-api.printify.com', // 🚀 Printify is now whitelisted!
+        hostname: 'images-api.printify.com', 
       }
     ],
   },
-};
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          }
+        ],
+      },
+    ];
+  },
+}
 
 export default nextConfig;
